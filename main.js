88e2c60a91d313f8809d727d6a561552e95b5f33
BharudeSahil <bharudesahil@gmail.com>
2025-01-09 15:15:44 +0530

    Assignment 18

--- main.js
+++ main.js
@@ -1,45 +1,65 @@
-// Import the necessary modules
-const readline = require('readline');
+import chalk from "chalk";
+import readline from "readline";
 
-// Create a list of blocked URLs
+const log = console.log;
 const blockedUrls = [
-  'http://example.com',
-  'http://blocked.com',
-  'http://malicious.com',
+  "https://www.google.com",
+  "https://www.youtube.com",
+  "https://www.wikipedia.org",
+  "https://www.amazon.com",
+  "https://www.facebook.com",
+  "https://twitter.com",
+  "https://www.instagram.com",
+  "https://www.linkedin.com",
 ];
 
-// Function to validate a URL
 function isValidUrl(url) {
   try {
-    new URL(url); // Attempt to construct a URL object
-    return true;
+    const parsedUrl = new URL(url);
+    return parsedUrl.protocol === "http:" || parsedUrl.protocol === "https:";
   } catch (err) {
     return false;
   }
 }
 
-// Function to check if a URL is blocked
 function isBlockedUrl(url) {
-  return blockedUrls.includes(url);
+  return blockedUrls.includes(url.toLowerCase());
 }
 
-// Create an interface for user input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Prompt user for a URL
-rl.question('Enter a URL to validate: ', (inputUrl) => {
-  // Analyze and validate the URL
+rl.question("Enter a URL to validate: ", (inputUrl) => {
+  if (!inputUrl || inputUrl.trim() === "") {
+    console.error("Error: No URL provided.");
+    rl.close();
+    return;
+  }
+  const url = inputUrl.trim();
   if (!isValidUrl(inputUrl)) {
-    console.error('Error: The URL is not valid.');
+    console.error("Error: The URL is not valid.");
   } else if (isBlockedUrl(inputUrl)) {
-    console.warn('Warning: The URL is blocked.');
+    console.warn(chalk.red("Warning: The URL is blocked."));
   } else {
-    console.log('Success: The URL is valid and accessible.');
+    console.log(chalk.green("Success: The URL is valid and accessible."));
+    try {
+      const url = new URL(inputUrl);
+      const params = url.searchParams;
+
+      if (params.toString() === "") {
+        console.log(chalk.blue("No parameter found in this URL."));
+      } else {
+        console.log(chalk.blue(`There are parameters in this URL:`));
+        params.forEach((value, key) => {
+          console.log(`${key}: ${value}`);
+        });
+      }
+    } catch (error) {
+      console.error(chalk.red("Error: The URL is not valid."));
+    }
   }
 
-  // Close the input stream
   rl.close();
 });
